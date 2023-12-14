805a963ab37435fd53a5b700035df0d120d1c9e2
Carel Osagi <145139982+CarelOsagi@users.noreply.github.com>
2023-12-14 22:27:15 +0700

    Add files via upload

---
+++ Job Sheet 1 -  Algoritma, Pemrograman  IDE/1/Hello World!.c
@@ -0,0 +1,18 @@
+//Created by Carel Habsian Osagi (23343061) Informatika'23
+/*bagian header dari pemrograman, #include <stdio.h> adalah bagian library
+pemrograman bahasa c*/
+#include <stdio.h>
+
+/*bagian utama dari pemrograman yang berisi perintah dari programmer berupa input
+dan ouput*/
+void main(){
+	
+	/*printf() merupakan perintah untuk menampilkan output ke layar komputer,
+	printf("")merupakan perintah output menampilkan text*/
+	printf("Hello World!");
+	
+	/*Return 0; merupakan akhir dari fungsi main serta mengembalikan nilai 0
+	menunjukkan bahwa program telah berjalan dengan sukses tanpa ada masalah. 
+	Ini adalah konvensi yang umum digunakan dalam banyak sistem operasi.*/
+	return 0;
+}
